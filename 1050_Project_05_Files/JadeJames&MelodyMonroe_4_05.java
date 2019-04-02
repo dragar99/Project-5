1fb11782c38d04cbdac8bea835bae90d9785344e
mnmonroe <mnmonroe@gmail.com>
2019-04-02 17:40:57 -0600

    Add files via upload
    Added some framework to main

--- 1050_Project_05_Files/JadeJames&MelodyMonroe_4_05.java
+++ 1050_Project_05_Files/JadeJames&MelodyMonroe_4_05.java
@@ -1,6 +1,18 @@
-public class JadeJames&MelodyMonroe_4_05 {
+   import java.util.Scanner;  // Access the Scanner class
+   import java.io.*;          // Access PrintWriter and related classes
+   import java.io.FileReader; // Access FileReader
+   import java.text.DecimalFormat; // Access decimal format class
+   import java.util.ArrayList; // Access array list class
 
+
+public class JadeJames&MelodyMonroe_4_05 {
+   
    public static void main(String[ ] args) throws IOException {
+
+      // Declarations
+      final String EMPLOYEE_INPUT  = "EmployeeInput.txt";
+      final String EMPLOYEE_PARAMETERS = "EmployeeParameters.txt";
+      final String OUTPUT_FILE = "JadeJames&MelodyMonroe_4_05_Output.txt";   
    
    } // End main  
 } // End class   

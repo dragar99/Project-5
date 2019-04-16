fe313a1aa79b152be5a425b910d51e0763895610
mnmonroe <mnmonroe@gmail.com>
2019-04-16 17:49:53 -0600

    Update JadeJames&MelodyMonroe_4_05.java

--- JadeJames&MelodyMonroe_4_05.java
+++ JadeJames&MelodyMonroe_4_05.java
@@ -5,7 +5,7 @@
    import java.util.ArrayList; // Access array list class
 
 
-public class JadeJames&MelodyMonroe_4_05 {
+public class JadeJamesMelodyMonroe_4_05 {
    
    public static void main(String[ ] args) throws IOException {
 
@@ -13,6 +13,27 @@ public class JadeJames&MelodyMonroe_4_05 {
       final String EMPLOYEE_INPUT  = "EmployeeInput.txt";
       final String EMPLOYEE_PARAMETERS = "EmployeeParameters.txt";
       final String OUTPUT_FILE = "JadeJames&MelodyMonroe_4_05_Output.txt";   
+   
+      public void grossPay(int basePay, int hours) {
+         
+         int hours = 0;
+         int basePay = 10;
+         int grossPay = 0;
+         
+         if (hours <= 40) {
+            grossPay = hours * basePay;
+         }
+         
+         else if (hours <= 50) {
+            grossPay = (40 * basePay) + ((hours - 40) * (basepay * 1.5));
+         } 
+         
+         else {
+            grossPay = (40 * basePay) + ((50 - 40) * (basepay * 1.5)) + ((hours - 50) * (basepay * 2.0));
+         } 
+         
+      } //end grossPay
+
    
    } // End main  
 } // End class   
